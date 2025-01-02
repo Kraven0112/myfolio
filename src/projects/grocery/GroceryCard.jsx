3da5bd93f5aea9f9666bbe1393fa862c1cf3ac30
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-01-02 11:46:29 +0530

    halka

--- src/projects/grocery/GroceryCard.jsx
+++ src/projects/grocery/GroceryCard.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../../contexts/ThemeContext'
 export default function GroceryCard({title,image}) {
     const[isDark] = useContext(ThemeContext)
   return (
-    <Link to={`/grocerydetail/${title}`} className={`block m-1 text-center rounded-2xl w-[full] h-[200px] sm:w-[340px] sm:h-[410px] sm:p-1 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
+    <Link to={`/grocerydetail/${title}`} className={`block m-2 text-center rounded-2xl w-[full] h-[200px] sm:w-[340px] sm:h-[410px] sm:m-0 sm:p-1 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
     <img className=' w-full h-[70%] sm:w-full sm:h-[90%]' src={image} alt="accessory" />
     <h1 className='sm:mt-1 sm:text-2xl text-xl font-bold'>{title}</h1>
   </Link>
