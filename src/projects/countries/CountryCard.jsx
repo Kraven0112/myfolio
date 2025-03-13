ddb71fd747e0afa930b7b73ea40dd9a181b50ed7
Kraven0112 <sushilchaudhary2059@gmail.com>
2025-03-13 18:53:23 +0530

    add new

--- src/projects/countries/CountryCard.jsx
+++ src/projects/countries/CountryCard.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../../contexts/ThemeContext'
 export default function CountryCard({title,image,capital,continent,region}) {
     const[isDark] = useContext(ThemeContext)
   return (
-    <Link to={`/country/${title}`} className={`block overflow-hidden rounded-xl md:rounded-2xl sm:w-[300px] marker md:w-[340px] md:h-[415px] hover:scale-[1] my-2 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
+    <Link to={`/country/${title}`} className={`block overflow-hidden rounded-xl md:rounded-2xl sm:w-[300px] md:w-[340px] md:h-[415px] hover:scale-[1] my-2 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
       <img className=' w-[100%] md:h-[200px] h-[110px]' src={image} alt="accessory" />
       <div className='md:mt-3 p-1 md:p-2 md:space-y-3'>
       <h1 className='sm:mt-1 md:text-4xl text-[12px] font-bold'>{title}</h1>
